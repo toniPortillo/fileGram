0c12e3816c43d7f3ed55cda5a8d28781a68314fe
toni <toniplsec@gmail.com>
2017-05-04 13:04:56 +0200

    Arreglado el problema de seguridad que permitia meter comandos a personas externas

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const getFile = require('./getFile');
 const situation = require('./situation.js');
 
 const TOKEN = process.env.TOKEN;
+const IDTEL = process.env.IDTEL;
 const bot = new telegraf(TOKEN);
 
 let situationsValue = ' ';
@@ -17,115 +18,118 @@ let count;
 let countGet;
 let format;
 
+
 bot.on('text', function (ctx) {
-  const instruction = ctx.update.message.text;
-  count = instruction.split('cd /');
-  countGet = instruction.split('get ');
-  format = ctx.update.message.text;
+  if(ctx.message.from.id == IDTEL) {
+    const instruction = ctx.update.message.text;
+    count = instruction.split('cd /');
+    countGet = instruction.split('get ');
+    format = ctx.update.message.text;
 
-  if(count[0] === '') {
-    cd_aux = count[1];
-    console.log(cd_aux);
-  }else if(countGet[0] === '') {
-    cd_aux = 'get';
-    console.log(cd_aux);
-  }else {
-    if(format[0] === '/') {
-      cd_aux = '';
+    if(count[0] === '') {
+      cd_aux = count[1];
+      console.log(cd_aux);
+    }else if(countGet[0] === '') {
+      cd_aux = 'get';
+      console.log(cd_aux);
     }else {
-      cd_aux = instruction;
+      if(format[0] === '/') {
+        cd_aux = '';
+      }else {
+        cd_aux = instruction;
+      }
     }
-  }
-  console.log(ctx.update.message.text);
-  console.log(cd_aux);
+    console.log(ctx.update.message.text);
+    console.log(cd_aux);
 
-  if(countGet[0] === '') {
-    situationsValue = situation(ctx.update.message.text, aux);
-  }else {
-    situationsValue = situation(cd_aux, aux);
-    aux = situationsValue;
-  }
-  console.log(aux);
+    if(countGet[0] === '') {
+      situationsValue = situation(ctx.update.message.text, aux);
+    }else {
+      situationsValue = situation(cd_aux, aux);
+      aux = situationsValue;
+    }
+    console.log(aux);
 
-  switch(cd_aux) {
-    case 'cd':
-      ctx.reply(situationsValue);
-      cd(function (error, data){
-        if(error){
-          console.error('error', error);
-        }else {
-          data.forEach(function (file){
-            ctx.reply(file);
-          });
-        }
-      });
-    break;
-
-    case 'cd ..':
-      ctx.reply(situationsValue);
-      cdReturnBack(situationsValue, function(error, data) {
-        if(error) {
-          console.error('error', error);
-        }else {
-          data.forEach(function (file) {
-            ctx.reply(file);
-          });
-        }
-      });
-    break;
-
-    case 'get':
-      ctx.reply(situationsValue);
-      getFile(situationsValue, function (error, data) {
-        if(error){
-          console.error(situationsValue);
-          ctx.reply(situationsValue + ' no corresponde con ningún formato de fichero valido');
-        }else {
-          bot.telegram.sendDocument('11150012', {
-            source: situationsValue
-          })
-        }
-      });
-
-    break;
-
-    case 'ls':
-      ctx.reply(situationsValue);
-      ls(situationsValue, function (error, data) {
-        if(error){
-          console.error('error', error);
-          let format = situationsValue.indexOf('/cd ');
-          if(format != -1) {
-            ctx.reply('Comando incorrecto, introduzca comando cd o cd / (ruta completa)')
+    switch(cd_aux) {
+      case 'cd':
+        ctx.reply(situationsValue);
+        cd(function (error, data){
+          if(error){
+            console.error('error', error);
           }else {
-            ctx.reply('Comando incorrecto, Path:' + situationsValue + ' no existe, introduce comando cd o cd / (ruta completa)');
+            data.forEach(function (file){
+              ctx.reply(file);
+            });
           }
-        }else {
-          data.forEach(function (file) {
-            ctx.reply(file);
-          });
-        }
-      });
-    break;
+        });
+      break;
 
-    default:
-    ctx.reply(situationsValue);
-      cdRute(situationsValue, function (error, data) {
-        if(error){
-          console.error('error', error);
-          let format = situationsValue.indexOf('/cd ');
-          if(format != -1) {
-            ctx.reply('Comando incorrecto, introduzca comando cd o cd / (ruta completa)')
+      case 'cd ..':
+        ctx.reply(situationsValue);
+        cdReturnBack(situationsValue, function(error, data) {
+          if(error) {
+            console.error('error', error);
           }else {
-            ctx.reply('Comando incorrecto, Path:' + situationsValue + ' no existe, introduce comando cd o cd / (ruta completa)');
+            data.forEach(function (file) {
+              ctx.reply(file);
+            });
           }
-        }else{
-          data.forEach(function (file){
-            ctx.reply(file);
-          });
-        }
-      });
-    break;
+        });
+      break;
+
+      case 'get':
+        ctx.reply(situationsValue);
+        getFile(situationsValue, function (error, data) {
+          if(error){
+            console.error(situationsValue);
+            ctx.reply(situationsValue + ' no corresponde con ningún formato de fichero valido');
+          }else {
+            bot.telegram.sendDocument('11150012', {
+              source: situationsValue
+            })
+          }
+        });
+
+      break;
+
+      case 'ls':
+        ctx.reply(situationsValue);
+        ls(situationsValue, function (error, data) {
+          if(error){
+            console.error('error', error);
+            let format = situationsValue.indexOf('/cd ');
+            if(format != -1) {
+              ctx.reply('Comando incorrecto, introduzca comando cd o cd / (ruta completa)')
+            }else {
+              ctx.reply('Comando incorrecto, Path:' + situationsValue + ' no existe, introduce comando cd o cd / (ruta completa)');
+            }
+          }else {
+            data.forEach(function (file) {
+              ctx.reply(file);
+            });
+          }
+        });
+      break;
+
+      default:
+      ctx.reply(situationsValue);
+        cdRute(situationsValue, function (error, data) {
+          if(error){
+            console.error('error', error);
+            let format = situationsValue.indexOf('/cd ');
+            if(format != -1) {
+              ctx.reply('Comando incorrecto, introduzca comando cd o cd / (ruta completa)')
+            }else {
+              ctx.reply('Comando incorrecto, Path:' + situationsValue + ' no existe, introduce comando cd o cd / (ruta completa)');
+            }
+          }else{
+            data.forEach(function (file){
+              ctx.reply(file);
+            });
+          }
+        });
+      break;
+    }
   }
 })
 
